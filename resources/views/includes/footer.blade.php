8834c284d9a8adfdd0a426ed6b383a0aee363d6f
kitlabs <kitlabs1@gmail.com>
2019-01-25 18:48:43 +0530

    First Commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,4 @@
+<hr>
+<p class="text-center">
+    &copy; {{ date('Y') }}. All Rights are reserved to {{ app('global_settings')[0]['setting_value'] }}. created by <a href="http://krescentglobal.com/" target="_blank">Kitlab</a>
+</p>
